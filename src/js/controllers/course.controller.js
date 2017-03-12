00f9253848187ad841515bb1afa136c947077d03
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-03-11 22:37:17 -0300

    Adding erase button and adding style

--- src/js/controllers/course.controller.js
+++ src/js/controllers/course.controller.js
@@ -58,4 +58,21 @@ angular.module('cadeasalaAdmin.course', [])
         }
       )
     }
+
+    $scope.eraseCourseDisciplinesData = function() {
+      _.each($scope.courseDisciplines, function(courseDiscipline) {
+        courseDiscipline.ap1_local = null
+        courseDiscipline.ap1_date = null
+        courseDiscipline.ap2_local = null
+        courseDiscipline.ap2_date = null
+        courseDiscipline.ap3_local = null
+        courseDiscipline.ap3_date = null
+        courseDiscipline.ad1_deadline = null
+        courseDiscipline.ad2_deadline = null
+        courseDiscipline.presencial_tutor = null
+        courseDiscipline.tutorship_room = null
+        courseDiscipline.tutorship_time = null
+        courseDiscipline.tutorship_weekday = null
+      })
+    }
   }])
